965d7633c27cd01a2469b84c27c687fd1034c1d4
sahil-kaler <119391867+sahil-kaler@users.noreply.github.com>
2023-06-05 16:05:49 +0530

    commit

---
+++ Factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <conio.h>
+int main()
+{
+int n,f=1,i=1;
+printf("Enter a number: ");
+scanf("%d",&n);
+while(i<=n)
+ {
+ f = f * i;
+ i++;
+ }
+printf("\n Factorial of %d = %d", n,f);
+return 0;
+}
